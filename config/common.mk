deba455b508400c339f0130741064a21ebf7c1ec
Chet Kener <Cl3Kener@gmail.com>
2014-12-03 21:09:44 -0500

    Add new Squisher/Opticharger for Lollipop (2/2)
    notice:
    -------
    
    needs pngquant (best tool) install on linux system for this feature to work
    
    p4:
       use ascii color codes instead of tput. Its more universal
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Change-Id: Ib89addf8cba8a7a48e94df3d9e077b1526d9829c
    
    @fusionjack:
    * Move scripts to tools folder
    * Define the SQUISHER_SCRIPT
    * Slim-ify
    * Colorize opticharger
    * Force only using pngquant
    
    Change-Id: If877d6f27790e49d7476702c0a7af9ce736741b6
    
    Conflicts:
    	config/common.mk
    	tools/opticharger
    	tools/squisher

--- config/common.mk
+++ config/common.mk
@@ -329,6 +329,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 -include vendor/sokp-priv/keys/keys.mk
 
+SQUISHER_SCRIPT := vendor/spirit/tools/squisher
+
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
